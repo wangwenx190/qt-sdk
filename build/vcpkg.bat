a2df2b34efeed526364b010f1138ee8593157909
Zhao Yuhang <2546789017@qq.com>
2024-04-13 14:39:58 +0800

    update vcpkg script

--- build/vcpkg.bat
+++ build/vcpkg.bat
@@ -7,15 +7,16 @@ set __vcpkg_dir=%~dp0..\vcpkg
 set __triplet_dir=%~dp0vcpkg-custom-triplets
 set __qt_deps=openssl icu
 set __vcpkg_triplets=x64-windows-shared-mt-release x64-windows-static-mt-release
+set __debug_flags=--debug --debug-env --printmetrics
 cd /d "%__vcpkg_dir%"
 call bootstrap-vcpkg.bat
 cd /d "%__vcpkg_dir%"
 :: Always try to update the libraries to the latest version.
-vcpkg update
+vcpkg update %__debug_flags%
 :: Without the "--no-dry-run" parameter, VCPKG won't upgrade
 :: the installed libraries in reality.
-vcpkg upgrade --no-dry-run
-for %%i in (%__vcpkg_triplets%) do vcpkg install %__qt_deps% --triplet=%%i --overlay-triplets="%__triplet_dir%"
+vcpkg upgrade %__debug_flags% --no-dry-run
+for %%i in (%__vcpkg_triplets%) do vcpkg install %__qt_deps% --triplet=%%i --overlay-triplets=%__triplet_dir% %__debug_flags%
 endlocal
 cd /d "%~dp0"
 pause
