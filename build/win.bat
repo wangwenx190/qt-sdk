2443c5bf99df8fb50e148f8beca780e178b4c9ed
Zhao Yuhang <2546789017@qq.com>
2025-06-17 16:44:11 +0800

    adapt to mingw

--- build/win.bat
+++ build/win.bat
@@ -3,6 +3,8 @@ chcp 65001
 title Building Qt SDK ...
 setlocal enabledelayedexpansion
 cls
+cd /d "%~dp0."
+if exist env.bat call env.bat
 set __compiler=%1
 set __type=%2
 set __config=%3
@@ -41,7 +43,7 @@ if /i "%__clangcl%" == "1" (
     set __install_dir=%__install_dir%_clangcl
 )
 if /i "%__mingw%" == "1" (
-    set __platform=%__platform%-clang-g++
+    set __platform=%__platform%-g++
     set __cmake_dir=%__cmake_dir%_mingw
     set __install_dir=%__install_dir%_mingw
 )
@@ -64,29 +66,29 @@ if /i "%__static%" == "1" (
     set __vcpkg_install_dir=%__vcpkg_install_dir%-shared-mt-release
 )
 :: OpenSSL
-set OpenSSL_DIR=%__vcpkg_install_dir%
-set OpenSSL_ROOT=%OpenSSL_DIR%
-set OPENSSL_ROOT_DIR=%OpenSSL_ROOT%
-set OPENSSL_BIN_DIR=%OPENSSL_ROOT_DIR%\bin
-set OPENSSL_INCLUDE_DIR=%OPENSSL_ROOT_DIR%\include
-set OPENSSL_LIB_DIR=%OPENSSL_ROOT_DIR%\lib
-set OPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIB_DIR%\libcrypto.lib
-set OPENSSL_SSL_LIBRARY=%OPENSSL_LIB_DIR%\libssl.lib
-set OPENSSL_INCLUDE_DIRS=%OPENSSL_INCLUDE_DIR%
-set OPENSSL_LIBRARIES=%OPENSSL_CRYPTO_LIBRARY%;%OPENSSL_SSL_LIBRARY%
+::set OpenSSL_DIR=%__vcpkg_install_dir%
+::set OpenSSL_ROOT=%OpenSSL_DIR%
+::set OPENSSL_ROOT_DIR=%OpenSSL_ROOT%
+::set OPENSSL_BIN_DIR=%OPENSSL_ROOT_DIR%\bin
+::set OPENSSL_INCLUDE_DIR=%OPENSSL_ROOT_DIR%\include
+::set OPENSSL_LIB_DIR=%OPENSSL_ROOT_DIR%\lib
+::set OPENSSL_CRYPTO_LIBRARY=%OPENSSL_LIB_DIR%\libcrypto.lib
+::set OPENSSL_SSL_LIBRARY=%OPENSSL_LIB_DIR%\libssl.lib
+::set OPENSSL_INCLUDE_DIRS=%OPENSSL_INCLUDE_DIR%
+::set OPENSSL_LIBRARIES=%OPENSSL_CRYPTO_LIBRARY%;%OPENSSL_SSL_LIBRARY%
 :: ICU
-set ICU_DIR=%__vcpkg_install_dir%
-set ICU_ROOT=%ICU_DIR%
-set ICU_ROOT_DIR=%ICU_ROOT%
-set ICU_BIN_DIR=%ICU_ROOT_DIR%\bin
-set ICU_INCLUDE_DIR=%ICU_ROOT_DIR%\include
-set ICU_LIB_DIR=%ICU_ROOT_DIR%\lib
-set ICU_I18N_LIBRARY=%ICU_LIB_DIR%\icuin.lib
-set ICU_UC_LIBRARY=%ICU_LIB_DIR%\icuuc.lib
-set ICU_DATA_LIBRARY=%ICU_LIB_DIR%\icudt.lib
-set ICU_IO_LIBRARY=%ICU_LIB_DIR%\icuio.lib
-set ICU_INCLUDE_DIRS=%ICU_INCLUDE_DIR%
-set ICU_LIBRARIES=%ICU_I18N_LIBRARY%;%ICU_UC_LIBRARY%;%ICU_DATA_LIBRARY%;%ICU_IO_LIBRARY%
+::set ICU_DIR=%__vcpkg_install_dir%
+::set ICU_ROOT=%ICU_DIR%
+::set ICU_ROOT_DIR=%ICU_ROOT%
+::set ICU_BIN_DIR=%ICU_ROOT_DIR%\bin
+::set ICU_INCLUDE_DIR=%ICU_ROOT_DIR%\include
+::set ICU_LIB_DIR=%ICU_ROOT_DIR%\lib
+::set ICU_I18N_LIBRARY=%ICU_LIB_DIR%\icuin.lib
+::set ICU_UC_LIBRARY=%ICU_LIB_DIR%\icuuc.lib
+::set ICU_DATA_LIBRARY=%ICU_LIB_DIR%\icudt.lib
+::set ICU_IO_LIBRARY=%ICU_LIB_DIR%\icuio.lib
+::set ICU_INCLUDE_DIRS=%ICU_INCLUDE_DIR%
+::set ICU_LIBRARIES=%ICU_I18N_LIBRARY%;%ICU_UC_LIBRARY%;%ICU_DATA_LIBRARY%;%ICU_IO_LIBRARY%
 :: -vcpkg: we need this parameter to enable VCPKG integration, but here we don't need VCPKG, because we have set the required CMake variables already.
 set __config_params=-platform %__platform% -prefix "%__install_dir%" -nomake tests -nomake examples -icu -feature-relocatable -feature-c++20 -verbose
 set __build_target=install
@@ -120,7 +122,8 @@ if /i "%__clangcl%" == "1" set __need_vs=1
 if /i "%__need_vs%" == "1" call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 set __qt_dir=%~dp0..
 set __qtbase_dir=%__qt_dir%\qtbase
-set PATH=%OPENSSL_BIN_DIR%;%ICU_BIN_DIR%;%__qtbase_dir%\bin;%PATH%
+::set PATH=%OPENSSL_BIN_DIR%;%ICU_BIN_DIR%;%__qtbase_dir%\bin;%PATH%
+set PATH=%__qtbase_dir%\bin;%PATH%
 cd /d "%~dp0."
 if exist %__cmake_dir% rd /s /q %__cmake_dir%
 if exist %__install_dir% rd /s /q %__install_dir%
@@ -128,24 +131,24 @@ md %__cmake_dir%
 cd %__cmake_dir%
 ::set QT_ENABLE_VCLTL=0
 ::set QT_ENABLE_YYTHUNKS=0
-if /i "%__debug%" == "0" (
-    set QT_ENABLE_VCLTL=1
-    set QT_ENABLE_YYTHUNKS=1
-)
+::if /i "%__debug%" == "0" (
+::    set QT_ENABLE_VCLTL=1
+::    set QT_ENABLE_YYTHUNKS=1
+::)
 call "%__qt_dir%\configure.bat" %__config_params%
 cmake %__build_params%
-if /i "%__static%" == "0" (
-    copy /y "%OPENSSL_BIN_DIR%\*.dll" "%__install_dir%\bin"
-    copy /y "%ICU_BIN_DIR%\*.dll" "%__install_dir%\bin"
-)
-copy /y "%OPENSSL_LIB_DIR%\*.lib" "%__install_dir%\lib"
-copy /y "%ICU_LIB_DIR%\*.lib" "%__install_dir%\lib"
-xcopy "%OPENSSL_INCLUDE_DIR%" "%__install_dir%\include" /s /i /f /r /y
-xcopy "%ICU_INCLUDE_DIR%" "%__install_dir%\include" /s /i /f /r /y
-cd /d "%~dp0."
-if exist %__install_dir%.7z del /f %__install_dir%.7z
-set __7z_params=-mx -myx -ms=on -mqs=on -mmt=on -m0=LZMA2:d=64m:fb=64
-7z a %__install_dir%.7z %__install_dir%\ %__7z_params%
+::if /i "%__static%" == "0" (
+::    copy /y "%OPENSSL_BIN_DIR%\*.dll" "%__install_dir%\bin"
+::    copy /y "%ICU_BIN_DIR%\*.dll" "%__install_dir%\bin"
+::)
+::copy /y "%OPENSSL_LIB_DIR%\*.lib" "%__install_dir%\lib"
+::copy /y "%ICU_LIB_DIR%\*.lib" "%__install_dir%\lib"
+::xcopy "%OPENSSL_INCLUDE_DIR%" "%__install_dir%\include" /s /i /f /r /y
+::xcopy "%ICU_INCLUDE_DIR%" "%__install_dir%\include" /s /i /f /r /y
+::cd /d "%~dp0."
+::if exist %__install_dir%.7z del /f %__install_dir%.7z
+::set __7z_params=-mx -myx -ms=on -mqs=on -mmt=on -m0=LZMA2:d=64m:fb=64
+::7z a %__install_dir%.7z %__install_dir%\ %__7z_params%
 endlocal
 cd /d "%~dp0."
 pause
